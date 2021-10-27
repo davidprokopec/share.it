e2b9475aac7a785dbb717bd141aa9daa6397203f
David Prokopec <41482710+davidprokopec@users.noreply.github.com>
2021-10-27 17:47:17 +0200

    finished register frontend, fixed sql backend

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+export const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            // prihlasenej
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="Username"
+              label="Username"
+            ></InputField>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="Password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
