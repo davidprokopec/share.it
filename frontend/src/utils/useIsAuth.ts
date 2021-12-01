0c4bc790e7d8b01c85ebc6e0ce561f5b067d55dc
David Prokopec <41482710+davidprokopec@users.noreply.github.com>
2021-12-01 01:08:16 +0100

    frontend - query routing

--- frontend/src/utils/useIsAuth.ts
+++ frontend/src/utils/useIsAuth.ts
@@ -7,7 +7,7 @@ export const useisAuth = () => {
   const router = useRouter();
   useEffect(() => {
     if (!fetching && !data?.me) {
-      router.replace("/login");
+      router.replace("/login?next=" + router.pathname);
     }
   }, [fetching, data, router]);
 };
