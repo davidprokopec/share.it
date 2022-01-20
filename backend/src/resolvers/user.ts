de3aff534c755f028640c698c8cf63aa14fcb520
David Prokopec <41482710+davidprokopec@users.noreply.github.com>
2022-01-20 22:42:35 +0100

    backend - secured password

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -6,10 +6,12 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  Root,
 } from "type-graphql";
 import argon2 from "argon2";
 import { UsernamePasswordInput } from "./UsernamePasswordInput";
@@ -34,8 +36,18 @@ class UserResponse {
   user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    // vlastni email - ukazat email
+    if (req.session.userId === user.id) {
+      return user.email;
+    }
+    // cizi email - ukazat email
+    return "";
+  }
+
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg("token") token: string,
