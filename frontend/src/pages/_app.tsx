3141d8fe35f3c0823dd07db53fdced6c6492db22
davidprokopec <prokopec.david@seznam.cz>
2022-11-28 22:55:39 +0100

    added faq

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ChakraProvider resetCSS theme={theme}>
+        <Head>
+          <title>Share.it</title>
+          <meta name="viewport" content="width=320, initial-scale=1"></meta>
+        </Head>
         <Component {...pageProps} />
       </ChakraProvider>
     </>
